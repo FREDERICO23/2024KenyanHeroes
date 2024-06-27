cb6b423f4e4bf20cac1a8ca561bba8013b7a56d5
FREDERICO23 <adhingafredrick@gmail.com>
2024-06-27 16:00:11 +0300

    Update build_files.sh to enable debugging and improve collectstatic command

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,6 @@
 # build_files.sh
+set -x  # Enable debugging
+echo "Installing requirements..."
 pip install -r requirements.txt
-python manage.py collectstatic
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
