075a46dedc08d47b6874381b30113ffc01d0d118
FREDERICO23 <adhingafredrick@gmail.com>
2024-06-27 15:32:10 +0300

    Update STATIC_ROOT path in settings.py and vercel.json

--- backend/settings.py
+++ backend/settings.py
@@ -144,8 +144,8 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # MEDIA CONFIG
